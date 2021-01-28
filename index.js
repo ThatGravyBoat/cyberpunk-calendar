d14ee28025a0c592d698e96c14b8f10934c085c7
ThatGravyBoat <gravyboat211@gmail.com>
2021-01-28 08:31:11 -0330

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 document.onload = loadFunction();
+var currentDay = 0;
+var currentMonth = 0;
+
+const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 function loadFunction() {
     setTime();
     repeatEvery(setTime, 1000);
 }
 
+function setCurrentDayAndMonth() {
+    document.getElementById("month").innerText = `${monthNames[currentMonth]}`
+    
+}
+
+function getDaysInMonth(month,year) { return new Date(year, month, 0).getDate() }
+
+
 function setTime() {
     var date = new Date();
+    if (currentDay < date.getDate() || currentMonth != date.getMonth()){
+        currentDay = date.getDate()
+        currentMonth = date.getMonth()
+
+
+
+    }
     var hours = date.getHours();
     hours = hours % 12;
     hours = hours ? hours : 12;
